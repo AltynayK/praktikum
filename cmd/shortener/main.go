4b19b1d24f75e445380766d885b6e5e6043b23dc
AltynayK <altynaykurmangaliyeva@gmail.com>
2022-03-28 23:20:21 +0600

    инкримент1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,39 @@
 package main
 
-func main() {}
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type MyUrl struct {
+	ShortUrl int
+	LongUrl  string
+}
+
+func PostEndpoint(w http.ResponseWriter, req *http.Request) {
+	var url MyUrl
+	_ = json.NewDecoder(req.Body).Decode(&url)
+	var aParams []interface{}
+	aParams = append(aParams, url.LongUrl)
+	for i := 0; i < len(aParams); i++ {
+		if aParams[i] == url.LongUrl {
+			url.ShortUrl = i
+		}
+	}
+	json.NewEncoder(w).Encode(url.ShortUrl)
+}
+
+func GetEndpoint(w http.ResponseWriter, req *http.Request) {
+	var aParams []interface{}
+	params := req.URL.Query()
+	aParams = append(aParams, params.Get("shortUrl"))
+}
+
+func main() {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", PostEndpoint)
+	mux.HandleFunc("/{id}", GetEndpoint)
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/AltynayK/praktikum
+
+go 1.13
+
+require honnef.co/go/tools v0.2.2
