732297f15d1bfffa4b288d6642ac691de3737d84
AltynayK <altynaykurmangaliyeva@gmail.com>
2022-03-30 21:23:42 +0600

    версия 2

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,39 +1,15 @@
 package main
 
 import (
-	"encoding/json"
+	"go/format"
 	"log"
-	"net/http"
+
+	"github.com/AltynayK/praktikum"
 )
 
-type MyUrl struct {
-	ShortUrl int
-	LongUrl  string
-}
-
-func PostEndpoint(w http.ResponseWriter, req *http.Request) {
-	var url MyUrl
-	_ = json.NewDecoder(req.Body).Decode(&url)
-	var aParams []interface{}
-	aParams = append(aParams, url.LongUrl)
-	for i := 0; i < len(aParams); i++ {
-		if aParams[i] == url.LongUrl {
-			url.ShortUrl = i
-		}
-	}
-	json.NewEncoder(w).Encode(url.ShortUrl)
-}
-
-func GetEndpoint(w http.ResponseWriter, req *http.Request) {
-	var aParams []interface{}
-	params := req.URL.Query()
-	aParams = append(aParams, params.Get("shortUrl"))
-}
-
 func main() {
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", PostEndpoint)
-	mux.HandleFunc("/{id}", GetEndpoint)
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	srv := new(praktikum.Server)
+	if err:=srv.Run(port: "8080"); err !=nil {
+		log.Fatal(format: "error occured while running http server: %s", err.Error())
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/AltynayK/praktikum
 
 go 1.13
 
-require honnef.co/go/tools v0.2.2
+
