54b94cccbc9d80f63693e152fce2c7b21e812f59
AltynayK <altynaykurmangaliyeva@gmail.com>
2022-03-31 14:52:35 +0600

    инкримент 1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,15 +1,44 @@
 package main
 
 import (
-	"go/format"
+	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
 
-	"github.com/AltynayK/praktikum"
+	"github.com/gorilla/mux"
 )
 
-func main() {
-	srv := new(praktikum.Server)
-	if err:=srv.Run(port: "8080"); err !=nil {
-		log.Fatal(format: "error occured while running http server: %s", err.Error())
+type MyUrl struct {
+	LongUrl  string `json:"LongUrl,omitempty"`
+	ShortUrl int    `json:"ShortUrl,omitempty"`
+}
+
+var n1qlParams []interface{}
+
+func PostEndpoint(w http.ResponseWriter, req *http.Request) {
+	var url MyUrl
+	_ = json.NewDecoder(req.Body).Decode(&url)
+
+	n1qlParams = append(n1qlParams, url.LongUrl)
+
+	url.ShortUrl = len(n1qlParams) - 1
+	err := json.NewEncoder(w).Encode(url.ShortUrl)
+	if err != nil {
+		fmt.Fprintf(w, "%s", err.Error())
 	}
+
+}
+
+func RootEndpoint(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/", PostEndpoint).Methods("POST")
+	router.HandleFunc("/{id}", RootEndpoint).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,10 @@
 module github.com/AltynayK/praktikum
 
-go 1.13
+go 1.18
 
 
+
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/speps/go-hashids v2.0.0+incompatible // indirect
+)
